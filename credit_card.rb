3c90799b20ac5ee32a5018da102e945eaf570dcc
Greg Molnar <molnargerg@gmail.com>
2014-02-05 10:33:28 +0100

    make sure it strips spaces

--- credit_card.rb
+++ credit_card.rb
@@ -2,6 +2,20 @@ class CreditCard
   attr_accessor :number
 
   def initialize(number)
-    self.number = number
+    self.number = number.gsub(/\s/, '')
+    #   AMEX       | 34 or 37    | 15            |
+    # +------------+-------------+---------------+
+    # | Discover   | 6011        | 16            |
+    # +------------+-------------+---------------+
+    # | MasterCard | 51-55       | 16            |
+    # +------------+-------------+---------------+
+    # | Visa       | 4           | 13 or 16
+    @types = {
+      amex: /^3[47]\d{13}$/
+    }
+  end
+
+  def type
+
   end
 end
--- test/credit_card_test.rb
+++ test/credit_card_test.rb
@@ -2,9 +2,29 @@ require 'minitest/autorun'
 require_relative '../credit_card'
 class CreditCardTest < MiniTest::Unit::TestCase
 
+	def setup
+		@cards = [
+			{number: '4111111111111111', type: 'VISA', valid: true},
+			{number: '4111111111111', type: 'VISA', valid: false},
+			{number: '4012888888881881', type: 'VISA', valid: true},
+			{number: '378282246310005', type: 'AMEX', valid: true},
+			{number: '6011111111111117', type: 'Discover', valid: true},
+			{number: '5105105105105100', type: 'MasterCard', valid: true},
+			{number: '5105105105105106', type: 'MasterCard', valid: false},
+			{number: '9111111111111111', type: 'Unknown', valid: false}
+		]
+	end
+
 	def test_it_accepts_the_number_at_initialize
 		number = '1234 1234 1234 1234'
 		card = CreditCard.new(number)
-		assert_equal card.number, number
+		assert_equal card.number, number.gsub(/\s/, '')
+	end
+
+	def test_it_extracts_type
+		@cards.each do |card|
+			c = CreditCard.new(card[:number])
+			assert_equal c.type, card[:type]
+		end
 	end
 end
